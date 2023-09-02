3a50b29e1b9876c94a5a8c4146b61aec7bf0a562
Pablo Carmona <pablocarben@outlook.com>
2023-09-02 23:52:33 +0200

    Backup

--- FSCmodelUnity/Assets/Scripts/CSVReader.cs
+++ FSCmodelUnity/Assets/Scripts/CSVReader.cs
@@ -12,27 +12,38 @@ public static class CSVReader
         int i = 0, j;       // Contadores para recorrer el array donde vamos a almacenar los valores.
         string[] values;    // Valores de las cadenas de cada línea del archivo .csv.
         string[,] valor;    // Array 2D de strings que contiene los valores del .csv organizados en matriz.
-        
-        int numberOfLines = File.ReadLines(filePath).Count();       //  Primero calculamos el número de líneas del archivo. En el caso
-                                                                    // de nuestro proyecto, eso equivale al número de muestras reales.
-        var firstLine = File.ReadLines(filePath).FirstOrDefault();  //  Ahora identificamos la primera línea.
-        var numberOfColumns = firstLine.Split(',').Length;          // Con el objetivo de calcular el número de columnas que tiene el
-                                                                    // archivo. Es decir, el número de variables medidas, en
-                                                                    // nuestro caso.
-        valor = new string[numberOfLines, numberOfColumns];         //  Inicializamos las dimensiones de nuestra variable "valor".
+
+        //  Primero calculamos el número de líneas del archivo. En el caso
+        // de nuestro proyecto, eso equivale al número de muestras reales.
+        int numberOfLines = File.ReadLines(filePath).Count();
+        //  Ahora identificamos la primera línea.
+        var firstLine = File.ReadLines(filePath).FirstOrDefault();
+        // Con el objetivo de calcular el número de columnas que tiene el
+        // archivo. Es decir, el número de variables medidas, en
+        // nuestro caso.
+        var numberOfColumns = firstLine.Split(',').Length;
+        //  Inicializamos las dimensiones de nuestra variable "valor".
+        valor = new string[numberOfLines, numberOfColumns];
 
         if (File.Exists(filePath))
         {
             using (StreamReader reader = new StreamReader(filePath))
             {
-                while (!reader.EndOfStream)             //  Mientras queden datos por leer:
+                //  Mientras queden datos por leer:
+                while (!reader.EndOfStream)
                 {
-                    string line = reader.ReadLine();    //  Leemos la línea en curso,
-                    values = line.Split(',');           // separamos los valores que contiene según las comas (importante que estén puestas),
-                    j = 0;                              // reseteamos j a 0 antes de entrar en el bucle de asignación.
-                    foreach (string value in values)    //  Como hemos separado los valores por las comas, ahora tenemos tantos strings
-                    {                                   // como columnas tenga el archivo.
-                        valor[i, j] = value;            //  Así que, cogemos cada string y lo asignamos a una posición del array 2D.
+                    //  Leemos la línea en curso,
+                    string line = reader.ReadLine();
+                    // separamos los valores que contiene según las comas (importante que estén puestas),
+                    values = line.Split(',');
+                    // reseteamos j a 0 antes de entrar en el bucle de asignación.
+                    j = 0;
+                    //  Como hemos separado los valores por las comas, ahora tenemos tantos strings
+                    // como columnas tenga el archivo.
+                    //  Así que, cogemos cada string y lo asignamos a una posición del array 2D.
+                    foreach (string value in values)
+                    {                                   
+                        valor[i, j] = value;            
                         j++;
                     }
                     i++;
